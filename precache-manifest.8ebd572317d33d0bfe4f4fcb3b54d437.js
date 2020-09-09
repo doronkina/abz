b6010a110d2b804d0dd117a79d1b053171d2dac4
doronkina <liudmyla.doronkina0@gmail.com>
2020-09-09 09:30:20 +0300

    Updates

---
+++ precache-manifest.8ebd572317d33d0bfe4f4fcb3b54d437.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93564e8bd76054053a183192ffef15ac",
+    "url": "/abz/index.html"
+  },
+  {
+    "revision": "6a67d2c3fe9b1c0ae17d",
+    "url": "/abz/static/css/main.908ee606.chunk.css"
+  },
+  {
+    "revision": "5b53f86931347f77f0dd",
+    "url": "/abz/static/js/2.c06f2948.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/abz/static/js/2.c06f2948.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a67d2c3fe9b1c0ae17d",
+    "url": "/abz/static/js/main.984cd7c5.chunk.js"
+  },
+  {
+    "revision": "9a3809aa0f664dfc6e69",
+    "url": "/abz/static/js/runtime-main.c53e6103.js"
+  },
+  {
+    "revision": "f5443d74025f2a6069e0ca0142c08a05",
+    "url": "/abz/static/media/banner-photo.f5443d74.jpg"
+  },
+  {
+    "revision": "146e29265bcc680129e263a9478dca00",
+    "url": "/abz/static/media/logo.146e2926.svg"
+  },
+  {
+    "revision": "3cdf78971c869b16b95152be7b6922e5",
+    "url": "/abz/static/media/man-laptop-v1.3cdf7897.svg"
+  }
+]);
